cd84dcda864c73060964c8f864b5b5024e975db9
Vladimir <w112vovan@mail.ru>
2023-11-27 22:33:14 +0500

    Правка 1

--- Program.cs
+++ Program.cs
@@ -12,16 +12,18 @@ namespace L16_multiples
         {
             Random random = new Random();
             int minNumber = 1;
-            int maxNumber = 28;
+            int maxNumber = 27;
             int minNumberOfCheck = 100;
             int maxNumberOfCheck = 1000;
             int countOfMultipleNumbers = 0;
-            int N = random.Next(minNumber, maxNumber);
+            int N = random.Next(minNumber, maxNumber + 1);
 
             for (int i = 0; i < maxNumberOfCheck; i += N)
             {
                 if (i >= minNumberOfCheck)
+                {
                     ++countOfMultipleNumbers;
+                }
             }
             Console.WriteLine($"Количество трехзначных чисел кратных {N}, составляет {countOfMultipleNumbers}\n");
         }
