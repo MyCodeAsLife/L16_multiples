7bb75872c16c7e1812580e67fd175b52d9f21347
Vladimir <w112vovan@mail.ru>
2023-11-27 13:54:21 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L16_multiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minNumber = 1;
+            int maxNumber = 28;
+            int minNumberOfCheck = 100;
+            int maxNumberOfCheck = 1000;
+            int countOfMultipleNumbers = 0;
+            int N = random.Next(minNumber, maxNumber);
+
+            for (int i = 0; i < maxNumberOfCheck; i += N)
+            {
+                if (i >= minNumberOfCheck)
+                    ++countOfMultipleNumbers;
+            }
+            Console.WriteLine($"Количество трехзначных чисел кратных {N}, составляет {countOfMultipleNumbers}\n");
+        }
+    }
+}
